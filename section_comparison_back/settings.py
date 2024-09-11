ebd1ab0c0d95297bc9f911a47d45feef99932628
Valentina Toni <vale.toni93@gmail.com>
2024-09-11 15:25:21 +0200

    DONE CRUD porjects, sections, blocks

--- section_comparison_back/settings.py
+++ section_comparison_back/settings.py
@@ -31,7 +31,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-# Application definition
 
 INSTALLED_APPS = [
     'corsheaders',
@@ -44,6 +43,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'djoser',
     'accounts',
+    'projects',
     'core'
 ]
 
